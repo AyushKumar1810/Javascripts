d4733efb636f82b238868ea63b8d88a5f252fe08
Ayush <ayushsri1810@gmail.com>
2024-01-13 10:42:20 +0530

    first

---
+++ app.js
@@ -0,0 +1,121 @@
+// console.log("Hello from ayush kumar to the world");
+// let total = 1+3;
+// console.log("Goodbye");
+// console.log("before condition");
+// let random = Math.random();
+// if (random < 0.5){
+//     console.log("Hey ! Your number is less than 0.5");
+//     console.log(random);
+// }
+// console.log("After conditions");
+
+// const dayofweek='Tuesday';
+// if (dayofweek==='Monday'){
+//     console.log("I really hate It");
+// }else if ( dayofweek==='Saturday'){
+//     console.log('I really love Saturday'); 
+// }else if(dayofweek==='Friday'){
+//     console.log("Friday is decent really after work ");
+// }
+
+// 0-5=> Free
+// 5-10 =>Child fare
+// 10-65 => Adult fare
+// 65+ => Senior 10$
+
+// const age = 10;
+// if (age <=5){
+//     console.log("Hey And welcome Your Entry is free");
+// }else if (age>5 & age<10){
+//     console.log("Hey And welcome Your Entry is Child fare");
+// }else if (age >=10 & age <65){
+//     console.log("Hey And welcome Your Entry is Adult Fare of 20$");
+// }else if (age>=65){
+//     console.log("Hey And welcome Your Entry is 10$");
+// }
+
+//NOTE: password must be 6+ Characters 
+//NOTE: password cannot include space
+// const password = prompt("Please Enter Your password");
+// if (password.length>=6){
+//     console.log("Your Password Is Good To go !");
+// }else if (password.length<6){
+//     console.log("Your password is Short");
+// }
+// if (password.indexOf(' ')===-1){
+//     console.log("your passsword doesn't contains space");
+
+// }else{
+//     console.log("Please remove space from your password");
+// }
+
+// TRUTHY AND FALSY VALUE
+
+// const userInput = prompt("Enter something");    
+// if (userInput ){
+//     console.log("TRUTHY!");
+// }else{
+//     console.log("FALSY!");
+// }
+
+//NOTE: AND Operator : Both left and right condition should be true 
+// ayush=true && true
+// console.log(ayush);
+// const password= prompt("Please enter your password ?")
+// if (password.length>=6 && password.indexOf(' ')===-1){
+//     console.log("Valid password");
+// }else{
+//     console.log("Please Enter valid password ");
+// }
+
+//                SWITCH Operator
+// const day =2
+// if (day==1){
+//     console.log("It's Monday");
+// }
+// else if (day==2){
+//     console.log("It's Tuesday");
+// }
+// else if (day==3){
+//     console.log("It's Wednesday");
+// }
+// else if (day==4){
+//     console.log("It's thrusday");
+// }
+// else if (day==5){
+//     console.log("It's Friday");
+// }
+// // if (day==6){
+// //     console.log("It's Saturday");
+// // }
+// // if (day==7){
+// //     console.log("It's Sunday");
+// // }
+// else{
+//     console.log("I DON'T KNOW THAT");}
+// const day = 30
+// switch (day){
+//     case 1 :
+//         console.log("MONDAY");
+//         break;
+//     case 2 :
+//         console.log("TUESDAY");
+//         break;
+//     case 3 :
+//         console.log("WEDNESDAY");
+//         break
+//     case 4 :
+//         console.log("THRUSDAY");
+//         break
+//     case 5 :
+//         console.log("FRIDAY");
+//         break;
+//     case 6 :
+//         console.log("SATURDAY");
+//         break
+//     case 7 :
+//         console.log("SUNDAY");
+//         break
+//     default:
+//         console.log("I Don't Know That ");
+// }
