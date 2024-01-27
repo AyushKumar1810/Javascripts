2f09a559b835db59d3d1277c85ddd4b2505536dd
Ayush <ayushsri1810@gmail.com>
2024-01-27 09:52:25 +0530

    For each

---
+++ For_each.js
@@ -0,0 +1,44 @@
+// const number=[1,2,3,4,5,6,7,8,9,0]
+// function print(elements){ // that will print 1,2,3,4,5,6,7,8,9,0
+//     console.log(elements);
+
+// }
+// number.forEach(print)
+
+// const number1=[1,2,3,4,5,6,7,8,9,0]
+// number.forEach(function(el){ //That will also print the same above 1 to 0
+//     console.log(el);
+// })
+
+// const number2=[1,2,3,4,5,6,7,8,9,0]
+
+// for (let el of number){
+//     console.log(el); // That will do the same 
+// }
+// const movies = [
+//     {
+//         title: 'Amadeus',
+//         score: 99
+//     },
+//     {
+//         title: 'Stand By Me',
+//         score: 85
+//     },
+//     {
+//         title: 'Parasite',
+//         score: 95
+//     },
+//     {
+//         title: 'Alien',
+//         score: 90
+//     } 
+// ]
+// movies.forEach(function(movie){
+//     console.log(movie);
+// })
+//////////MAPS 
+//NOTE: It just create a new array and return The value
+// const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+// numbers.map(function(num){
+//     return num*2
+// })
